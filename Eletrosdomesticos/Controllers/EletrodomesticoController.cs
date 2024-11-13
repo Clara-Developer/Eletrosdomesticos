efdf28ebd027ec580997541a743f5de57da2db9d
clara-developer <ana.c.silva381@aluno.senai.br>
2024-11-13 08:08:10 -0300

    Adicionar arquivos de projeto.

---
+++ Eletrosdomesticos/Controllers/EletrodomesticoController.cs
@@ -0,0 +1,20 @@
+﻿using Eletrosdomesticos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eletrosdomesticos.Controllers
+{
+    public class EletrodomesticoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Eletrodomestico> Eletros = new List<Eletrodomestico>
+            {
+                new Geladeira {Marca = "Brastemp", Modelo = "Frost free", ConsumoEnergetico = 70, CapacidadeLitros = 500, TemFreezer = true},
+                new Televisor {Marca = "Toshiba", Modelo = "Dled", ConsumoEnergetico = 88.5, TamanhoTela = 120, EhSmart = false},
+                new Televisor {Marca = "TCL", Modelo = "Ld 55", ConsumoEnergetico = 54.9, TamanhoTela = 72, EhSmart = false}
+            };
+            return View(Eletros);
+            
+        }
+    }
+}
---
+++ Eletrosdomesticos/Models/Geladeira.cs
@@ -0,0 +1,12 @@
+﻿namespace Eletrosdomesticos.Models
+{
+    public class Geladeira : Eletrodomestico
+    {
+        public int CapacidadeLitros { get; set; }
+        public bool TemFreezer { get; set; }
+        public override string ExibirDetalhes()
+        {
+            return $"Marca: {Marca}, Modelo: {Modelo}, ConsumoEnergetico: {ConsumoEnergetico}, CapacidadeLitros {CapacidadeLitros}, TemFreezer {TemFreezer}";
+        }
+    }
+}
